5912dc66cff2779fedae84e589f293f3eeb8348c
practicalswift <practicalswift@users.noreply.github.com>
2016-05-15 18:34:46 +0200

    Add licensing preamble.

--- crashes/28293-irgensilfunction-visitfullapplysite.swift
+++ crashes/28293-irgensilfunction-visitfullapplysite.swift
@@ -1,4 +1,10 @@
-// Distributed under the terms of the MIT license
+// This source file is part of the Swift.org open source project
+// Copyright (c) 2014 - 2016 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+
 // Submitted by https://github.com/deville (Andrii Chernenko)
 
 func f(value: Any) {
