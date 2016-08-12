5408bcb977dcd052a05fb245454ec64fe508614f
practicalswift <practicalswift@users.noreply.github.com>
2016-08-12 11:14:06 +0200

    '.A[' no longer crashing swiftc.

--- swiftc_golf.sh
+++ swiftc_golf.sh
@@ -55,7 +55,6 @@ test_crash_case() {
 echo "Crashing:"
 
 test_crash_case '!('
-test_crash_case '.A['
 test_crash_case '[_?,&_'
 test_crash_case 'nil?=\n&_,'
 test_crash_case '[1,{[1,{[]'
@@ -82,6 +81,7 @@ test_crash_case '(&.f>_'
 test_crash_case '()=()'
 test_crash_case '([_'
 test_crash_case '({[({_'
+test_crash_case '.A['
 test_crash_case '.{nil<{\n{'
 test_crash_case '[&{}false?'
 test_crash_case '[[map'
