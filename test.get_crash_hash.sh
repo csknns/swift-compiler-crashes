f0231d13d65e36ede5447d7d2a22f9c75f0a9d06
practicalswift <practicalswift@users.noreply.github.com>
2016-10-30 19:59:14 +0100

    Fix bug in crash hash calculation.

--- test.get_crash_hash.sh
+++ test.get_crash_hash.sh
@@ -1,10 +1,19 @@
+#!/bin/bash
+
 # Definition of crash uniqueness (improvements welcome!) …
 # A crash is treated as non-duplicate if it has an unique "crash hash" as computed by the following crash hash function:
 get_crash_hash() {
-  local compilation_output="$1"
-  local unreachable_message=$(grep "UNREACHABLE executed at " <<< ${compilation_output} | head -1)
-  local assertion_message=$(grep "Assertion " <<< "${compilation_output}" | tr ":" "\n" | grep "Assertion " | head -1)
-  local normalized_stack_trace="${unreachable_message}${assertion_message}$(grep -E '^[0-9]+ swift +0x[0-f]' <<< ${compilation_output} | head -1)"
+  local compilation_output
+  local unreachable_message
+  local assertion_message
+  local normalized_stack_trace
+  compilation_output="$1"
+  unreachable_message=$(grep "UNREACHABLE executed at " <<< "${compilation_output}" | head -1)
+  assertion_message=$(grep "Assertion " <<< "${compilation_output}" | tr ":" "\n" | grep "Assertion " | head -1)
+  normalized_stack_trace="${unreachable_message}${assertion_message}"
+  if [[ ${normalized_stack_trace} == "" ]]; then
+      normalized_stack_trace=$(grep -E '^[0-9]+ swift +0x[0-f]' <<< "${compilation_output}" | head -1)
+  fi
   if [[ ${normalized_stack_trace} == "" ]]; then
     crash_hash=""
   else
