a63f91f97b00892cfe7c90ca5897dd3c923e1667
practicalswift <practicalswift@users.noreply.github.com>
2016-11-20 08:43:11 +0100

    Add crash case crashes-fuzzing/28533-swift-unqualifiedlookup-unqualifiedlookup-swift-declname-swift-declcontext-swift

---
+++ crashes-fuzzing/28533-swift-unqualifiedlookup-unqualifiedlookup-swift-declname-swift-declcontext-swift.swift
@@ -0,0 +1,17 @@
+// This source file is part of the Swift.org open source project
+// Copyright (c) 2014 - 2016 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+
+// RUN: not --crash %target-swift-frontend %s -emit-ir
+protocol a
+class T:Range<T> A{var f= Int S{
+enum B<A}typealias d
+}struct g{}}typealias e:d
+{func 丏
+let c
+func b>{
+{
+class a(t=e}}typealia
