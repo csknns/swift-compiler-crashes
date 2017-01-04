10da207fba070f6ca15c84bc1565b0fb41b38792
practicalswift <practicalswift@users.noreply.github.com>
2017-01-04 12:45:55 +0100

    Remove crashes/ dir.
    Everything goes into crashes-fuzzing/ from now on to simplify things.
    Non-fuzzing crash cases will be marked with "// non-fuzz".

--- test.sh
+++ test.sh
@@ -181,12 +181,12 @@ main() {
   echo "Running tests against: ${swiftc_version}"
   echo "Usage: $0 [-v] [-q] [-c<columns>] [-l] [file ...]"
   local current_max_id
-  current_max_id=$(find crashes crashes-fuzzing crashes-duplicates fixed -name "?????-*.swift" | cut -f2 -d'/' | grep -E '^[0-9]+\-' | sort -n | cut -f1 -d'-' | sed 's/^0*//g' | tail -1)
+  current_max_id=$(find crashes-fuzzing crashes-duplicates fixed -name "?????-*.swift" | cut -f2 -d'/' | grep -E '^[0-9]+\-' | sort -n | cut -f1 -d'-' | sed 's/^0*//g' | tail -1)
   local next_id
   next_id=$((current_max_id + 1))
   echo "Adding a new test case? The crash id to use for the next test case is ${next_id}."
   local duplicate_bug_ids
-  duplicate_bug_ids=$(find crashes crashes-fuzzing crashes-duplicates fixed -name "?????-*.swift" | cut -f2 -d/ | cut -f1 -d'.' | sort | uniq | cut -f1 -d'-' | uniq -c | sed "s/^ *//g" | grep -E -v '^1 ' | cut -f2 -d" " | tr "\n" "," | sed "s/,$//g")
+  duplicate_bug_ids=$(find crashes-fuzzing crashes-duplicates fixed -name "?????-*.swift" | cut -f2 -d/ | cut -f1 -d'.' | sort | uniq | cut -f1 -d'-' | uniq -c | sed "s/^ *//g" | grep -E -v '^1 ' | cut -f2 -d" " | tr "\n" "," | sed "s/,$//g")
   if [[ ${duplicate_bug_ids} != "" ]]; then
     show_error "Duplicate bug ids: ${duplicate_bug_ids}. Please re-number to avoid duplicates."
     echo
@@ -197,9 +197,7 @@ main() {
   }
   local argument_files=$*
   if [[ ${argument_files} == "" ]]; then
-    run_tests_in_directory "Currently known crashes, set #1 (human reported crashes)" "./crashes"
-    run_tests_in_directory "Currently known crashes, set #2 (crashes found by fuzzing)" "./crashes-fuzzing"
-    # run_tests_in_directory "Currently known crashes (duplicates)" "./crashes-duplicates"
+    run_tests_in_directory "Currently known crashes" "./crashes-fuzzing"
     if [[ ${delete_dupes} == 1 || ${delete_fixed} == 1 ]]; then
       exit 0
     fi
