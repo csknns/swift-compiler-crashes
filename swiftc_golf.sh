a8515543e2ee0f7ae0d886dde71200a6925eb561
practicalswift <practicalswift@users.noreply.github.com>
2017-02-09 11:15:34 +0100

    Mark as fixed.

--- swiftc_golf.sh
+++ swiftc_golf.sh
@@ -46,7 +46,6 @@ test_crash_case() {
 
 echo "Crashing:"
 
-test_crash_case '#if0='
 test_crash_case '[.h=_'
 test_crash_case '&[_=(&_'
 test_crash_case 'nil?=nil'
@@ -57,6 +56,7 @@ echo "Fixed:"
 
 test_crash_case '!('
 test_crash_case '!(0^_{'
+test_crash_case '#if0='
 test_crash_case '&(-_'
 test_crash_case '&(<)==('
 test_crash_case '&(>,_'
