7118a01bfbefb078bdf46af059e97767e995959a
practicalswift <practicalswift@users.noreply.github.com>
2017-03-29 17:21:02 +0200

    Minimize crash case submitted by @benasher44

--- crashes-fuzzing/28726-nominaltypedecl-hasfixedlayout.swift
+++ crashes-fuzzing/28726-nominaltypedecl-hasfixedlayout.swift
@@ -1,7 +1,1 @@
-public struct T<Specific> {
-    public typealias Closure = (Specific) -> Void
-}
-
-let closure: T.Closure = { (input: String) in
-    print("\(input)")
-}
+struct T<b{typealias a=(}let:T.a=
